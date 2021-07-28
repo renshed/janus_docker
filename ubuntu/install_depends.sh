7822d859f710f187a62ce5a6b7b42b711db48c43
renshed <87469749+renshed@users.noreply.github.com>
2021-07-28 22:38:03 +0300

    Update install_depends.sh

--- ubuntu/install_depends.sh
+++ ubuntu/install_depends.sh
@@ -3,10 +3,16 @@ apt-get upgrade -y
 
 apt-get install -y aptitude nano wget git unzip
 
-aptitude install libmicrohttpd-dev libjansson-dev \
-	libssl-dev libsrtp-dev libsofia-sip-ua-dev libglib2.0-dev \
-	libopus-dev libogg-dev libcurl4-openssl-dev liblua5.3-dev \
-	libconfig-dev pkg-config gengetopt libtool automake \
-	make cmake nginx python3-dev python3-pip
-   
-apt-get install -y meson
+
+aptitude install -y build-essential libmicrohttpd-dev libjansson-dev \
+        libssl-dev libsrtp-dev libsofia-sip-ua-dev libglib2.0-dev \
+        libopus-dev libogg-dev libcurl4-openssl-dev liblua5.3-dev \
+        libconfig-dev pkg-config gengetopt libtool automake \
+        cmake libssl-dev
+
+apt-get install -y python3 python3-pip python3-setuptools \
+        python3-wheel ninja-build
+
+#apt-get install -y meson
+pip3 install meson
+#apt-get --only-upgrade install -y meson
