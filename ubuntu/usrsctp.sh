e9d04f61c7eb6dfd4fd989d67f498a113ddb4e0a
renshed <87469749+renshed@users.noreply.github.com>
2021-07-28 20:48:23 +0300

    Update usrsctp.sh

--- ubuntu/usrsctp.sh
+++ ubuntu/usrsctp.sh
@@ -2,6 +2,5 @@ cd ~
 git clone https://github.com/sctplab/usrsctp
 cd usrsctp
 ./bootstrap
-./configure --prefix=/usr
-make
-sudo make install
+./configure --prefix=/usr --disable-programs --disable-inet --disable-inet6
+make && make install
