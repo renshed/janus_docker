4ced26d332302420efbb3646b11bb17623e5cee8
renshed <87469749+renshed@users.noreply.github.com>
2021-07-28 20:46:50 +0300

    Update libsrtp.sh

--- ubuntu/libsrtp.sh
+++ ubuntu/libsrtp.sh
@@ -1,7 +1,6 @@
 cd ~
-git clone https://github.com/cisco/libsrtp.git
-cd libsrtp
-git checkout v2.0.0
+wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
+tar xfv v2.2.0.tar.gz
+cd libsrtp-2.2.0
 ./configure --prefix=/usr --enable-openssl
-make shared_library
-sudo make install
+make shared_library && sudo make install
