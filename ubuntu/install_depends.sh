b4bdd6d4d87729ab806c5a629ad1f6ca51b2d739
renshed <87469749+renshed@users.noreply.github.com>
2021-07-28 20:58:44 +0300

    Update install_depends.sh

--- ubuntu/install_depends.sh
+++ ubuntu/install_depends.sh
@@ -7,6 +7,6 @@ aptitude install libmicrohttpd-dev libjansson-dev \
 	libssl-dev libsrtp-dev libsofia-sip-ua-dev libglib2.0-dev \
 	libopus-dev libogg-dev libcurl4-openssl-dev liblua5.3-dev \
 	libconfig-dev pkg-config gengetopt libtool automake \
-   make cmake nginx python3-dev python3-pip
+	make cmake nginx python3-dev python3-pip
    
 apt-get install -y meson
