8621c5d14fd19481db526f6ee3c82c24823b059a
renshed <87469749+renshed@users.noreply.github.com>
2021-07-28 20:45:46 +0300

    Update install_depends.sh

--- ubuntu/install_depends.sh
+++ ubuntu/install_depends.sh
@@ -1,8 +1,12 @@
 apt-get update -y 
 apt-get upgrade -y
-apt-get install -y libmicrohttpd-dev libjansson-dev libnice-dev \
-   libssl-dev libsofia-sip-ua-dev libglib2.0-dev \
-   libopus-dev libogg-dev libini-config-dev libcollection-dev \
-   pkg-config gengetopt libtool automake
 
-apt-get install -y sudo make git doxygen graphviz cmake
+apt-get install -y aptitude nano wget git unzip
+
+aptitude install libmicrohttpd-dev libjansson-dev \
+	libssl-dev libsrtp-dev libsofia-sip-ua-dev libglib2.0-dev \
+	libopus-dev libogg-dev libcurl4-openssl-dev liblua5.3-dev \
+	libconfig-dev pkg-config gengetopt libtool automake \
+   make cmake nginx python3-dev python3-pip
+   
+apt-get install -y meson
